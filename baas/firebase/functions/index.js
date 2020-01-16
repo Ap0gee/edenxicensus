efc536e0a78e6c8c4ba89840cdacb1cd54b6046e
Sjshovan <Sjshovan@Gmail.com>
2020-01-16 03:04:49 -0500

    firebase functions

--- baas/firebase/functions/index.js
+++ baas/firebase/functions/index.js
@@ -7,18 +7,34 @@ admin.initializeApp();
 
 const axios = require('axios');
 const moment = require('moment');
+const _ = require('lodash');
+
+const now = function() { return moment().format("Y-M-D h:m:ss"); };
 
 exports.collectOnlineCharacters = functions.https.onRequest(async (req, res) => {
     axios.get('https://cors-anywhere.herokuapp.com/https://www.edenxi.com/ajax/onlinecharacters', {headers: {'X-Requested-With': 'XMLHttpRequest'}})
         .then(function (response) {
             response.data.characters.forEach(function(character, index) {
-                admin.database().ref('/characters/' + character.charname).set({
-                    id: character.charid,
-                    name: character.charname,
-                    last_updated: moment().format("Y-M-D h:m:ss")
+                let ref = admin.database().ref('/characters/' + character.charname);
+                ref.once('value').then(function(snapshot) {
+                    let charData = {
+                        id: character.charid,
+                        name: character.charname,
+                        last_updated: now(),
+                    };
+                    if (snapshot.exists()) {
+                        if (!snapshot.val().hasOwnProperty('last_read') || !snapshot.val().hasOwnProperty('needs_read')) {
+                            charData = _.merge(charData, {'last_read': now(), 'needs_read': true});
+                        }
+                        ref.update(charData);
+                    } else {
+                        ref.set(charData);
+                    }
                 });
             });
-            res.status(200).send('done ' + moment().format("Y-M-D h:m:ss"));
+        })
+        .then(function() {
+            res.status(200).send('done ' + now());
         })
         .catch(function (error) {
             console.log(error);
@@ -26,11 +42,32 @@ exports.collectOnlineCharacters = functions.https.onRequest(async (req, res) => 
         });
 });
 
-exports.collectCharacterProfiles = functions.https.onRequest(async (req, res) => {
-    admin.database().ref("/characters").get()
-        .then(function (snapshot) {
-            snapshot.forEach(function (doc) {
-                console.log(doc.id, doc.data());
-            })
+exports.updateOnlineCharacters = functions.https.onRequest(async(req, res)=> {
+    let ref = admin.database().ref('/characters');
+    ref.once('value').then(function(snapshot) {
+        let updates = _.mapValues(snapshot.val(), function(char) {
+            if ( ! char.hasOwnProperty('last_read') || ! char.hasOwnProperty('needs_read') ) {
+                return _.merge(char, {'needs_read': true} )
+            }
+            return char;
         });
+        ref.update(updates);
+    }).then(function () {
+        res.status(200).send('done ' + now());
+    });
+});
+
+exports.collectCharacterProfiles = functions.https.onRequest(async (req, res) => {
+    let ref = admin.database().ref('/characters');
+    ref.orderByChild('needs_read').equalTo(true).limitToFirst(60).once('value').then(function(snapshot) {
+        if (_.keys(snapshot.val()).length) {
+            console.log('has items, process.')
+            //collect profile for each char, flip needs_read and set last_read.
+        } else {
+            console.log('no items left, reset db')
+            //flip needs_read for every character.
+        }
+    }).then(function() {
+        res.status(200).send('done ' + now());
+    });
 });
--- src/services/firebase.service.js
+++ src/services/firebase.service.js
@@ -1,14 +1,14 @@
-import { firebase } from "@firebase/app";
-import "@firebase/firestore";
-
-
-import * as admin from 'firebase-admin';
+const firebase = require("firebase/app");
+require("firebase/auth");
+require("firebase/firestore");
+require("firebase/database");
 
 export default class FirebaseService {
     constructor(config) {
         this.config = config;
         this.app = firebase.initializeApp(this.config);
-        this.firestore =  this.app.firestore();
+        this.firestore = this.app.firestore();
+        this.database = this.app.database();
     }
 }
 
