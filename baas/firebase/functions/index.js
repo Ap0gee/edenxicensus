e4360d574b34a30c78fbc0b7d850356c9e2aa86b
Sjshovan <Sjshovan@Gmail.com>
2020-01-17 21:22:41 -0500

    continue firebase functions

--- baas/firebase/functions/index.js
+++ baas/firebase/functions/index.js
@@ -7,6 +7,9 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+/* Collect Online Characters
+=======================================================================================================================*/
+
 /**
  * Collect and store all online characters gathered from the remote resource.
  * @type {HttpsFunction}
@@ -17,12 +20,10 @@ exports.collectOnlineCharacters = functions.https.onRequest(async (req, res) => 
         .then(function (response) {
             let last_updated = h.now();
             let ref = admin.database().ref(`/data/characters/averages/online/${ h.now('Y-M-D') }/${ last_updated }`);
-            ref.once('value').then(function(snapshot) {
-                ref.set({
-                    unique: response.data.unique,
-                    online: response.data.characters.length,
-                    last_updated: last_updated,
-                });
+            ref.set({
+                unique: response.data.unique,
+                online: response.data.characters.length,
+                last_updated: last_updated,
             });
             response.data.characters.forEach(function(character, index) {
                 let last_updated = h.now();
@@ -33,13 +34,14 @@ exports.collectOnlineCharacters = functions.https.onRequest(async (req, res) => 
                         name: character.charname,
                         last_updated: last_updated,
                     };
-                    if (snapshot.exists()) {
+                    if ( ! snapshot.exists() ) {
+                        charData = _.merge(charData, {'last_read': last_updated, 'needs_read': true});
+                        ref.set(charData);
+                    } else {
                         if ( ! snapshot.val().hasOwnProperty('last_read') || ! snapshot.val().hasOwnProperty('needs_read')) {
                             charData = _.merge(charData, {'last_read': last_updated, 'needs_read': true});
                         }
                         ref.update(charData);
-                    } else {
-                        ref.set(charData);
                     }
                 });
             });
@@ -59,7 +61,7 @@ exports.collectOnlineCharacters = functions.https.onRequest(async (req, res) => 
  * Run every 15 minutes.
  * @type {CloudFunction<unknown>}
  */
-exports.collectOnlineCharactersScheduled = functions.pubsub.schedule('15 * * * *').onRun((context) => {
+exports.collectOnlineCharactersScheduled = functions.pubsub.schedule('*/15 * * * *').onRun((context) => {
     console.log('Invoking "collectOnlineCharacters" https function...');
     axios.get('https://us-central1-edenxicensus.cloudfunctions.net/collectOnlineCharacters', {headers: {'X-Requested-With': 'XMLHttpRequest'}})
         .then(function (response) {
@@ -71,65 +73,59 @@ exports.collectOnlineCharactersScheduled = functions.pubsub.schedule('15 * * * *
     return null;
 });
 
+/* Collect Character Profiles
+=======================================================================================================================*/
+
 /**
  * Collect and store character profiles gathered from the remote resource.
  * @type {HttpsFunction}
  */
 exports.collectCharacterProfiles = functions.https.onRequest(async (req, res) => {
+    console.log('Collecting character profiles...');
     let ref = admin.database().ref('/characters');
-    ref.orderByChild('needs_read').equalTo(true).limitToFirst(60).once('value')
+    ref.orderByChild('needs_read').equalTo(true).limitToLast(50).once('value')
         .then(function(snapshot) {
-            if (_.keys(snapshot.val()).length) {
-                console.log('has items, process.')
-                //collect profile for each char, flip needs_read and set last_read.
-            } else {
-                console.log('no items left, reset db')
-                //flip needs_read for every character.
+            let characters = snapshot.val();
+            if (_.keys(characters).length) {
+                _.values(characters).forEach(function (character, index) {
+                    setTimeout(async () => {
+                        axios.get(`https://edenxi.com/ajax/character?id=${ character.id }`, {headers: {'X-Requested-With': 'XMLHttpRequest'}})
+                            .then(function(response) {
+                                let ref = admin.database().ref(`/profiles/${ character.id }`);
+                                ref.set(_.merge(response.data, {'last_updated': h.now()}));
+                            })
+                            .then(function() {
+                                let ref = admin.database().ref(`/characters/${ character.name }`);
+                                ref.update({
+                                    'last_read': h.now(),
+                                    'needs_read': false
+                                });
+                            })
+                            .catch(function(error) {
+                                console.log(error);
+                            })
+                    }, 100);
+                });
             }
         })
         .then(function() {
-            res.status(200).send(`done : ${ h.now() }`);
-        });
-});
-
-/**
- * Gather and update character statistics.
- * @type {HttpsFunction}
- */
-exports.updateCharacterData = functions.https.onRequest(async (req, res) => {
-    console.log('Updating character data...');
-    let last_updated = h.now();
-    let characters = {};
-    let ref = admin.database().ref('/characters');
-
-    ref.once('value')
-        .then(function(snapshot) {
-            characters = snapshot.val();
-        })
-        .then(function () {
-            let ref = admin.database().ref(`/data/characters/averages/total/${h.now('Y-M-D')}/${last_updated}`);
-            ref.once('value')
-                .then(function (snapshot) {
-                    ref.set({
-                        total: _.keys(characters).length,
-                        last_updated: last_updated
-                    });
-                })
-        })
-        .then(function () {
             console.log('success');
-            res.status(200).send(`done : ${h.now()}`);
+            res.status(200).send(`done : ${ h.now() }`);
+        })
+        .catch(function (error) {
+            console.log(error);
+            res.status(500);
         });
 });
 
 /**
- * Run the "updateCharacterData" https function on a schedule.
- * Run every 15 minutes.
+ * Run the "collectCharacterProfiles" https function on a schedule.
+ * Run every 3 minutes.
  * @type {CloudFunction<unknown>}
  */
-exports.updateCharacterDataScheduled = functions.pubsub.schedule('15 * * * *').onRun((context) => {
-    console.log('Invoking "updateCharacterData" https function...');
-    axios.get('https://us-central1-edenxicensus.cloudfunctions.net/updateCharacterData', {headers: {'X-Requested-With': 'XMLHttpRequest'}})
+exports.collectCharacterProfilesScheduled = functions.pubsub.schedule('*/3 * * * *').onRun((context) => {
+    console.log('Invoking "collectCharacterProfiles" https function...');
+    axios.get('https://us-central1-edenxicensus.cloudfunctions.net/collectCharacterProfiles', {headers: {'X-Requested-With': 'XMLHttpRequest'}})
         .then(function (response) {
             console.log('success');
         })
@@ -138,6 +134,70 @@ exports.updateCharacterDataScheduled = functions.pubsub.schedule('15 * * * *').o
         });
     return null;
 });
+
+/* Update Character Data
+=======================================================================================================================*/
+
+/**
+ * Gather and update totals of collected resources.
+ * @type {HttpsFunction}
+ */
+exports.updateCollectionTotals = functions.https.onRequest(async (req, res) => {
+    console.log('Updating totals...');
+    let last_updated = h.now();
+
+    let characters = {};
+    let profiles = {};
+
+    let ref = admin.database().ref('/characters');
+    ref.once('value')
+        .then(function(snapshot) {
+            characters = snapshot.val();
+        })
+        .then(function () {
+            let ref = admin.database().ref(`/data/characters/averages/total/${h.now('Y-M-D')}/${last_updated}`);
+            ref.set({
+                total: _.keys(characters).length,
+                last_updated: last_updated
+            });
+        });
+
+    ref = admin.database().ref('/profiles');
+    ref.once('value')
+        .then(function(snapshot) {
+            profiles = snapshot.val();
+        })
+        .then(function () {
+            let ref = admin.database().ref(`/data/profiles/averages/total/${h.now('Y-M-D')}/${last_updated}`);
+            ref.set({
+                total: _.keys(profiles).length,
+                last_updated: last_updated
+            });
+        });
+
+    console.log('success');
+    res.status(200).send(`done : ${h.now()}`);
+});
+
+/**
+ * Run the "updateCharacterData" https function on a schedule.
+ * Run every 15 minutes.
+ * @type {CloudFunction<unknown>}
+ */
+exports.updateCollectionTotalsScheduled = functions.pubsub.schedule('*/15 * * * *').onRun((context) => {
+    console.log('Invoking "updateCollectionTotals" https function...');
+    axios.get('https://us-central1-edenxicensus.cloudfunctions.net/updateCollectionTotals', {headers: {'X-Requested-With': 'XMLHttpRequest'}})
+        .then(function (response) {
+            console.log('success');
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    return null;
+});
+
+/* Calculate Averages
+=======================================================================================================================*/
 
 /**
  * Calculate and update character averages.
